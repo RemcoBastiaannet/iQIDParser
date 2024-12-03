76bfa4b45b54620b4c45174a7cb8083e99c859e8
NouranRRZaid <nzaid1@jh.edu>
2024-12-02 19:14:55 -0500

    Coregistration of PAS-stained tissue sections/ One section per slide

--- alphaCameraIntegration.py
+++ alphaCameraIntegration.py
@@ -10,17 +10,21 @@ from MicroscopeFileReader.MicroscopeFileReader import readTiff
 
 from MicroscopeFileReader import ImageManipulation
 
-Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
+Tk().withdraw()
 
+############## NZ: Required changes for PAS stain ############################################################################################
 
 fIQID = askdirectory(title='Select iQID listmode directory')
 fPhaloidin = askopenfilename(title='Select Phaloidin scan [stitched]')
 fDAPI = askopenfilename(title='Select DAPI scan [stitched]')
-# show an "Open" dialog box and return the path to the selected file
 fOutputDir = askdirectory(title='Select output directory', )
+#################
+# fIQID = askdirectory(title='Select iQID listmode directory')
+# fPAS = askopenfilename(title='Select PAS scan [stitched]')
+# fOutputDir = askdirectory(title='Select output directory', )
+##########################################################################################################################################
 
 
-# %% load files
 correctionTrans = [sitk.ReadParameterFile(fr'C:\OUTPUT\iQID Coreg\TransRectifyMeasurement_{ix}.txt') for ix in range(2)]
 
 iQID = iQIDParser(fIQID)
@@ -30,33 +34,39 @@ sitk.WriteImage(alphaImg, pjoin(fOutputDir, 'alphaCameraImg.nii'))
 alphaImg = sitk.Transformix(alphaImg, correctionTrans)
 sitk.WriteImage(alphaImg, pjoin(fOutputDir, '_alphaCameraImg.nii'))
 
-# load Microscope image
+
+
+############## NZ: Required changes for PAS stain ############################################################################################
 phaloidin = readTiff(fPhaloidin)
 sitk.WriteImage(phaloidin, pjoin(fOutputDir, 'PhaloidinImage.nii'))
-
 DAPI = readTiff(fDAPI)
+#####################
+# PAS = readTiff(fPAS)
+# sitk.WriteImage(PAS, pjoin(fOutputDir, 'PASImage.nii'))
+#############################################################################################################################################
 
-# coregister alpha and phaloidin
-alphaImg = doFullCoreg(alphaImg, phaloidin) #resulting in alphaImg in phaloidin
+
+
+############## NZ: Required changes for PAS stain ############################################################################################
+alphaImg = doFullCoreg(alphaImg, phaloidin)
 sitk.WriteImage(alphaImg, pjoin(fOutputDir, 'alphaCameraImgInMicroscopy.nii'))
 
-# Remove alpha fiduicial markers
-# alphaImg = ImageManipulation.removeFiducialMarkersAlpha(alphaImg)
-
-# Get bounding boxes for sections
 boundingBoxes = ImageManipulation.getObjectBoundingBoxes(phaloidin)
 
-phaloidinCuts = ImageManipulation.applyObjectBounsdingBoxes(
-    phaloidin, boundingBoxes)
+phaloidinCuts = ImageManipulation.applyObjectBounsdingBoxes(phaloidin, boundingBoxes)
 DAPICuts = ImageManipulation.applyObjectBounsdingBoxes(DAPI, boundingBoxes)
 
-alphaImgCuts = ImageManipulation.applyObjectBounsdingBoxes(
-    alphaImg, (np.array(boundingBoxes) * phaloidin.GetSpacing()[0] / alphaImg.GetSpacing()[0]).astype(int))
+alphaImgCuts = ImageManipulation.applyObjectBounsdingBoxes(alphaImg, (np.array(boundingBoxes) * phaloidin.GetSpacing()[0] / alphaImg.GetSpacing()[0]).astype(int))
 
-# # Coregister every section together
-# doFullAnatomicalCoreg(phaloidinCuts, DAPICuts,
-#                       alphaImgCuts, outputDir=fOutputDir)
+ImageManipulation.createComposites(phaloidinCuts, DAPICuts, alphaImgCuts, outputDir=fOutputDir)
+##################
+# alphaImg = doFullCoreg(alphaImg, PAS) 
+# sitk.WriteImage(alphaImg, pjoin(fOutputDir, 'alphaCameraImgInMicroscopy.nii'))
+# boundingBoxes = ImageManipulation.getObjectBoundingBoxes(phaloidin)
 
-#  Create nice Composite images
-ImageManipulation.createComposites(
-    phaloidinCuts, DAPICuts, alphaImgCuts, outputDir=fOutputDir)
+#PASCuts = ImageManipulation.applyObjectBounsdingBoxes(PAS, boundingBoxes)
+#alphaImgCuts = ImageManipulation.applyObjectBounsdingBoxes(alphaImg, (np.array(boundingBoxes) * PAS.GetSpacing()[0] / alphaImg.GetSpacing()[0]).astype(int))
+
+#ImageManipulation.createComposites(PASCuts, alphaImgCuts, outputDir=fOutputDir)
+
+#############################################################################################################################################
